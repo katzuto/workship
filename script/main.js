96a15bc88776adfbdaa947829be2eb48437b1491
katzuto <vstepovoi@gmail.com>
2023-03-29 11:14:17 +0300

    work input

---
+++ script/main.js
@@ -0,0 +1,31 @@
+inputText = document.querySelector(".input-title")
+newTextInDiv = document.querySelector(".text-title")
+newDivContent = document.querySelector(".text-content")
+
+
+// func add value from input to newDiv
+
+// const changeValueInputToDiv = () => {
+//     value = inputText.value;
+//     function newTextDiv () {
+//         newTextInDiv = value;
+//         newTextDiv.style.display = block;
+//         inputText.style.display = none;
+//     }
+// }
+
+// inputText.addEventListener("keydown", changeValueInputToDiv)
+
+function changeValue () {
+    value = inputText.value
+    newTextInDiv.innerHTML = `${inputText.value}`
+    inputText.style.display = "none";
+    newDivContent.style.display = "block"
+}
+
+
+inputText.addEventListener("keypress", (e) => {
+    if (e.keyCode == 13) {
+        changeValue()
+    }
+});
