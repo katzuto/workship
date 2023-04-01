0bbdfc9754b8948491b33a2ac6860d5366b70eeb
katzuto <vstepovoi@gmail.com>
2023-04-01 10:06:51 +0300

    buttonnotwork

--- script/main.js
+++ script/main.js
@@ -3,7 +3,6 @@ newTextInDiv = document.querySelector(".text-title")
 newDivContent = document.querySelector(".text-content")
 
 
-
 // change upper input into div
 function changeValue() {
     value = inputText.value
@@ -33,21 +32,72 @@ newDivContent.addEventListener("click", () => {
 });
 
 
-//add note with template
-const checklist = document.querySelector('.checklist');
-const templateText = document.querySelector('#template-text');
-const inputNote = document.querySelector('.input-note')
 
-function addNoteText() {
-    const item = templateText.content.cloneNode(true);
-    item.querySelector('span').textContent = `${inputNote.value}`
-    checklist.append(item);
-    inputNote.value = '';
-};
+// const checklist = document.querySelector('.checklist');
+// const templateText = document.querySelector('#template-text');
+const listOfToDos = document.querySelector(".list-of-todos")
+const inputNote = document.querySelector('.input-note')
+let toDoList = [];
+
+
+
+
+// function addNoteText() {
+//     const item = templateText.content.cloneNode(true);
+//     item.querySelector('span').textContent = `${inputNote.value}`
+//     checklist.append(item);
+//     inputNote.value = '';
+// };
+
+
+// создаем новый объект который включает в себя написанное значение инпута
+function addNewToDoToArray() {
+    const newToDo = {
+        name: inputNote.value,
+        checked: false,
+    }
+    toDoList.push(newToDo); // после ввода инпута объект со значением инпута добавляется в массив методом push()
+    displayMessages()
+}
+
+// функция вывода сообщения из инпута в браузер
+function displayMessages() {
+    let displayMessage = ""; // создали пустую строку которая у нас будет отвечать за вывод сообщения в браузер
+    toDoList.forEach(function(item, index) {
+        displayMessage += `
+            <li>
+                <input type="checkbox" id="item_${index}" ${item.checked? "checked" : ""}>
+                <label for="item_${index}">${item.name}</label>
+                <button class="btn_${index}">delete</button>
+            </li>
+        ` // сочетаем строки стоит обратить на это внимание
+        listOfToDos.innerHTML = displayMessage;
+    })// метод forEach перебирает каждое значение массива и берет в себя коллбек функцию и применяет эту функцию к каждому элементу массива
+}
+
+
+
+
+
+// удаление таски
+function deleteTask() {
+    toDoList.forEach(function(item, index) {
+        toDoList.splice(index, 1)
+    })
+}
+
+
+
 
 inputNote.addEventListener("keypress", (e) => {
     if (e.keyCode == 13) {
-        addNoteText();
-        console.log('sndvjn');
+        addNewToDoToArray()
     }
 });
+
+
+
+
+
+// добавление localstorage
+
